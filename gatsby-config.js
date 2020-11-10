8d915d21b1a23b2ddde898cc2b5d03870704b0d1
Stephan Burger <stephanburger54@gmail.com>
2020-11-10 20:50:39 +0200

    config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 const path = require('path')
 
 module.exports = {
+  siteMetadata: {
+    title: "I am Stephan",
+    titleTemplate: "%s | Fullstack Engineer",
+    description: "I am Stephan, a fullstack engineer.",
+    url: "https:iamstephan.dev",
+    image: "", // Path to your image you placed in the 'static' folder
+    // twitterUsername: "",
+  },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
@@ -12,7 +20,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
     // {
     //   resolve: "gatsby-plugin-react-svg",
     //   options: {
@@ -20,6 +28,12 @@ module.exports = {
     //       include: /assets/
     //     }
     //   }
-    // }
+    // },
+    {
+      resolve: "gatsby-plugin-typography",
+      options: {
+        pathToConfigModule: "src/foundation/typography"
+      }
+    }
   ],
 }
