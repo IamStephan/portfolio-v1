1d9aa966401c4af884a6b0bd8cdc0e9a192fed07
Stephan Burger <stephanburger54@gmail.com>
2020-11-10 20:51:47 +0200

    added seo foundation component

---
+++ src/foundation/seo/index.js
@@ -0,0 +1,67 @@
+import React from "react"
+
+// SEO
+import { Helmet } from "react-helmet"
+
+// Router
+import { useLocation } from "@reach/router"
+
+// Gatsby
+import { useStaticQuery, graphql } from "gatsby"
+
+// Static query
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        defaultTitle: title
+        titleTemplate
+        defaultDescription: description
+        siteUrl: url
+        defaultImage: image
+      }
+    }
+  }
+`
+
+const SEO = ({ title, description, image, article }) => {
+  const { pathname } = useLocation()
+  const { site } = useStaticQuery(query)
+
+  const {
+    defaultTitle,
+    titleTemplate,
+    defaultDescription,
+    siteUrl,
+    defaultImage,
+  } = site.siteMetadata
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: `${siteUrl}${image || defaultImage}`,
+    url: `${siteUrl}${pathname}`,
+  }
+
+  return (
+    <Helmet title={seo.title} titleTemplate={titleTemplate}>
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+
+      {seo.url && <meta property="og:url" content={seo.url} />}
+
+      {(article ? true : null) && <meta property="og:type" content="article" />}
+
+      {seo.title && <meta property="og:title" content={seo.title} />}
+
+      {seo.description && (
+        <meta property="og:description" content={seo.description} />
+      )}
+
+      {seo.image && <meta property="og:image" content={seo.image} />}
+    </Helmet>
+  )
+}
+
+export default SEO
+
