21daa32f067e1791a70820023a318fb226b00a77
Stephan Burger <stephanburger54@gmail.com>
2021-01-11 23:34:02 +0200

    new cta section

--- src/sections/cta/index.js
+++ src/sections/cta/index.js
@@ -1,87 +1,111 @@
 import React from 'react'
 
+// Material
+import { Typography, Button } from '@material-ui/core'
+
+// Svgs
+import Brain from '../../assets/svg/abstracts/brain.svg'
+import Speaker from '../../assets/svg/abstracts/speaker.svg'
+import KingVert from '../../assets/svg/abstracts/king_vert.svg'
+import KingHor from '../../assets/svg/abstracts/king_hor.svg'
+
+
 // Templates
 import Section from '../../templates/page_section'
-
-// Foundation
-import Icon from '../../foundation/icons'
-
-// Material
-import { Button } from '@material-ui/core'
-
-// Gatbsy
-import { useStaticQuery, graphql, Link } from 'gatsby'
-import Img from 'gatsby-image/withIEPolyfill'
-
-// Svg
-import HeyThere from '../../assets/svg/text/hey_there.svg'
-import LetsGo from '../../assets/svg/text/letsgo.svg'
 
 // Styles
 import styles from './styles.module.scss'
 
-const query = graphql`
-  {
-    hand: file(relativePath: {eq: "cta/hand.png"}) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`
-
 const CTA = () => {
-  const { hand: { childImageSharp: { fluid: handImg } } } = useStaticQuery(query)
-
   return (
     <Section
       className={styles['ctaSection']}
     >
       <div
-        className={styles['showcaseContainer']}
+        className={styles['actionContainer']}
       >
-        <LetsGo
-          className={styles['letsgo']}
+        <div
+          className={styles['showcaseContainer']}
+        >
+          <Brain
+            className={styles['showcase']}
+          />
+        </div>
+
+        <Typography
+          variant='h4'
+          className={styles['title']}
+        >
+          Hire me
+        </Typography>
+
+        <Typography
+          className={styles['content']}
+        >
+          I am currently available for hire, either freelancing or full-time.
+        </Typography>
+
+        <Button
+          variant='contained'
+          color='secondary'
+          size='large'
+        >
+          My Resume
+        </Button>
+      </div>
+
+      <div
+        className={styles['dividerContainer']}
+      >
+        <KingVert
+          className={styles['dividerVert']}
         />
 
-        <Img
-          fluid={handImg}
-          draggable={false}
-          objectFit='contain'
-          className={styles['handImg']}
-          style={{
-            position: 'absolute'
-          }}
+        <KingHor
+          className={styles['dividerHor']}
         />
+
+        <div
+          className={styles['vsContainer']}
+        >
+          <Typography
+            className={styles['vs']}
+          >
+            <b>V.S</b>
+          </Typography>
+        </div>
       </div>
 
       <div
         className={styles['actionContainer']}
       >
         <div
+          className={styles['showcaseContainer']}
+        >
+          <Speaker
+            className={styles['showcase']}
+          />
+        </div>
+        <Typography
+          variant='h4'
+          className={styles['title']}
+        >
+          Contact Me
+        </Typography>
+
+        <Typography
           className={styles['content']}
         >
-          <HeyThere
-            className={styles['heyThere']}
-          />
+          Do you have an exciting new idea, or have a problem with your current software solutions.
+        </Typography>
 
-          <div
-            className={styles['action']}
-          >
-            <Button
-              variant='contained'
-              color='primary'
-              size='large'
-              component={Link}
-              to='/contact'
-              endIcon={<Icon name='door-open' className={styles['icon']} />}
-            >
-              Contact Me
-            </Button>
-          </div>
-        </div>
+        <Button
+          variant='contained'
+          color='secondary'
+          size='large'
+        >
+          Let's talk
+        </Button>
       </div>
     </Section>
   )
