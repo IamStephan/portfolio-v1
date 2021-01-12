dfd4f637586f03e4003f9c016948fad4d3db402f
Stephan Burger <stephanburger54@gmail.com>
2021-01-12 18:49:00 +0200

    featured blog section added

--- src/sections/blog_featured/index.js
+++ src/sections/blog_featured/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
+// Gatsby
+import { Link } from 'gatsby'
+
+// Foundation
+import Icon from '../../foundation/icons'
+
 // Material
-import { Typography } from '@material-ui/core'
+import { Typography, Button, Chip } from '@material-ui/core'
 
 // Organisms
 import Section from '../../organisms/page_section'
@@ -11,8 +17,31 @@ import styles from './styles.module.scss'
 
 const DemoArticles = [
   {
+    title: 'Let\'s explore microservices.',
+    summary: 'This can be an intimidating topic to discus but with good reason, it\'s complicated. Why not get fimiliar with it and start playing with the idea of creating a distributed system.',
+    date: Date.now(),
+    tags: [
+      'Thoughts',
+      'Code'
+    ],
+    link: 'askdmalksdm',
+    showcase: 'ausdgh aosdj apodj'
+  },
+  {
+    title: 'Understanding the ',
+    summary: 'lsa dj lasjd als d',
+    date: Date.now(),
+    tags: [
+      'Thoughts',
+      'Code',
+    ],
+    link: 'askdmalksdm',
+    showcase: 'ausdgh aosdj apodj'
+  },  
+  {
     title: 'Understanding the poops of poosps',
     summary: 'lsa dj lasjd als d',
+    date: Date.now(),
     tags: [
       'Thoughts',
       'Code'
@@ -32,16 +61,82 @@ const BlogPreview = () => {
       >
         <Typography
           variant='h2'
-          className={styles['title']}
+          className={styles['header']}
         >
           Featured Articles
         </Typography>
         <Typography
-          className={styles['subtitle']}
+          className={styles['subheader']}
         >
-          asdjsa dnlas da sdk aksd a dmasn dmasn dmna sdmna dmnas dmnas dans dabdobwq od jpodj owiqhd oi
+          Exploring uncontroversial topics to more guide-like article. See what catches your eye, maybe we can talk about it...
         </Typography>
+
+        <Button
+          variant='contained'
+          color='primary'
+          component={Link}
+          to='/articles'
+          className={styles['ctaAction']}
+          endIcon={(
+            <Icon
+              className={styles['icon']}
+              
+              name='eye'
+            />
+          )}
+        >
+          View all
+        </Button>
       </div>
+
+      {
+        DemoArticles.map(article => (
+          <div
+            key={article.title}
+            className={styles['articleContainer']}
+          >
+            <Typography
+              variant='overline'
+              className={styles['date']}
+            >
+              13 Nov
+            </Typography>
+
+            <Link
+              to={article.link}
+            >
+              <Typography
+                className={styles['title']}
+                variant='h5'
+              >
+                {article.title}
+              </Typography>
+            </Link>
+            
+            <div
+              className={styles['tagsContainer']}
+            >
+              {
+                article.tags.map(tag => (
+                  <Chip
+                    className={styles['tag']}
+                    key={tag}
+                    label={tag}
+                    size='small'
+                    color='default'
+                  />
+                ))
+              }
+            </div>
+
+            <Typography
+              className={styles['summary']}
+            >
+              {article.summary}
+            </Typography>
+          </div>
+        ))
+      }
     </Section>
   )
 }
