538e12257781263fadbe6da77cccb2fb87b27652
Stephan Burger <stephanburger54@gmail.com>
2021-01-13 22:05:06 +0200

    added contact section

--- src/sections/contact_form/index.js
+++ src/sections/contact_form/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-// SVGs
-import LetsGetInTouch from '../../assets/svg/abstracts/lets_get_in_touch.svg'
+// Foundation
+import Icon from '../../foundation/icons'
+
+// Material
+import { Typography, Button, TextField, Select } from '@material-ui/core'
 
 // Organisms
 import Section from '../../organisms/page_section'
+
+// Svgs
+import Sitting from '../../assets/svg/abstracts/sitting.svg'
+import City from '../../assets/svg/abstracts/city.svg'
 
 // Styles
 import styles from './styles.module.scss'
@@ -12,30 +19,123 @@ import styles from './styles.module.scss'
 const ContactForm = () => {
   return (
     <Section
-      className={styles['contactSection']}
+      className={styles['contactFormSection']}
     >
       <div
-        className={styles['showcaseContainer']}
+        className={styles['contactContainer']}
       >
-        <LetsGetInTouch
-          className={styles['showcase']}
-        />
-      </div>
+         <div
+            className={styles['formContainer']}
+          >
+            <div
+              className={styles['contentContainer']}
+            >
+              <Icon
+                name='volume-vibrate'
+                className={styles['icon']}
+              />
+              <Typography
+                variant='h4'
+                className={styles['title']}
+              >
+                Give me a shout...
+              </Typography>
+            </div>
+            
 
-      <div
-        className={styles['contactFormContainer']}
-      >
-        <div
-          className={styles['details']}
-        >
-          Details
-        </div>
+            <form
+              className={styles['form']}
+            >
+              <Select
+                native
+                variant='outlined'
+                className={styles['input']}
+              >
+                <option>
+                  Freelancing
+                </option>
 
-        <div
-          className={styles['form']}
-        >
-          form
-        </div>
+                <option>
+                  Job Opportunity
+                </option>
+
+                <option>
+                  Article | Case Study
+                </option>
+
+                <option>
+                  Issue with something
+                </option>
+
+                <option>
+                  Other
+                </option>
+              </Select>
+
+              <TextField
+                variant='outlined'
+                color='primary'
+                label='Email Address'
+                className={styles['input']}
+              />
+
+              <TextField
+                variant='outlined'
+                color='primary'
+                label='Your message'
+                className={styles['input']}
+                multiline
+                rows='4'
+              />
+
+              <Button
+                variant='contained'
+                color='primary'
+              >
+                Submit
+              </Button>
+            </form>
+
+            
+          </div>
+
+          <div
+            className={styles['showcaseContainer']}
+          >
+            <Typography
+              variant='h1'
+              className={styles['header']}
+            >
+              Let's work together on something <u>great!</u>
+            </Typography>
+
+            <Typography
+              className={styles['subheader']}
+              variant='h6'
+            >
+              Don't hesitate to send me a message or call me,
+              we can build on your proposition together and find the best possible outcome.
+            </Typography>
+
+            <div
+              className={styles['actionContainer']}
+            >
+              <Button
+                variant='outlined'
+                color='secondary'
+                size='large'
+                className={styles['action']}
+              >
+                My Resume
+              </Button>
+            </div>
+
+            <div
+              className={styles['showcase']}
+            >
+              <Sitting />
+            </div>
+          </div>
       </div>
     </Section>
   )
