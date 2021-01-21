8beb66f2d5df77b92f85c246f5cf5767ddf9a8d6
Stephan Burger <stephanburger54@gmail.com>
2021-01-21 10:54:28 +0200

    started working on 404 page

---
+++ src/pages/404/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Organisms
+import Page from '@organisms/page'
+
+const NotFound = () => {
+  return (
+    <Page>
+      asd
+    </Page>
+  )
+}
+
+export default NotFound
