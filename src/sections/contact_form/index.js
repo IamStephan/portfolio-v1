f1c7095956338824345b44b0ab0d7503996d90e7
Stephan Burger <stephanburger54@gmail.com>
2021-01-22 17:58:46 +0200

    contact form foundation

--- src/sections/contact_form/index.js
+++ src/sections/contact_form/index.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+
+import { encode } from '@utils/encodeFormData'
+
+// Hooks
+import { useForm } from '@hooks/useForm'
 
 // Foundation
 import Icon from '@foundation/icons_svg'
@@ -15,7 +20,48 @@ import { ReactComponent as Sitting } from '@svg/abstracts/sitting.svg'
 // Styles
 import styles from './styles.module.scss'
 
+const formFields = {
+  topic: 'topic',
+  email: 'email',
+  message: 'message'
+}
+
 const ContactForm = () => {
+  const formRef = useRef(null)
+
+  const initialValues = {
+    [formFields.topic]: 'general',
+    [formFields.email]: '',
+    [formFields.message]: ''
+  }
+
+  
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { handleSubmit, handleChange, resetForm, values } = useForm({
+    initialValues,
+    onSubmit: _handleInternalSubmit
+  })
+
+  function _handleInternalSubmit (values) {
+    setIsLoading(true)
+    fetch('/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        'form-name': 'contact-form',
+        ...values
+      })
+    }).then(() => {
+      resetForm()
+    }).catch(() => {
+
+    }).finally(() => {
+      setIsLoading(false)
+    })
+  }
+
+
   return (
     <Section
       className={styles['contactFormSection']}
@@ -42,59 +88,93 @@ const ContactForm = () => {
               </Typography>
             </div>
             
-
             <form
               className={styles['form']}
+              data-netlify='true'
+              method='POST'
+              name='Contact Form'
+              onSubmit={handleSubmit}
+              ref={formRef}
             >
               <Select
-                native
-                variant='outlined'
                 className={styles['input']}
+                disabled={isLoading}
+                name={formFields.topic}
+                native
+                onChange={handleChange}
+                required
+                value={values[formFields.topic]}
+                variant='outlined'
               >
-                <option>
+                <option
+                  value='general'
+                >
                   General
                 </option>
 
-                <option>
+                <option
+                  value='job_opportunity'
+                >
                   Job Opportunity
                 </option>
 
-                <option>
+                <option
+                  value='freelancing'
+                >
                   Freelancing
                 </option>
 
-                <option>
+                <option
+                  value='post_related'
+                >
                   Article | Case Study
                 </option>
 
-                <option>
+                <option
+                  value='issue'
+                >
                   Issue with something
                 </option>
 
-                <option>
+                <option
+                  value='other'
+                >
                   Other
                 </option>
               </Select>
 
               <TextField
-                variant='outlined'
-                color='primary'
-                label='Email Address'
                 className={styles['input']}
+                color='primary'
+                disabled={isLoading}
+                label='Email Address'
+                name={formFields.email}
+                onChange={handleChange}
+                required
+                type='email'
+                value={values[formFields.email]}
+                variant='outlined'
               />
 
               <TextField
-                variant='outlined'
-                color='primary'
-                label='Your message'
                 className={styles['input']}
+                color='primary'
+                disabled={isLoading}
+                label='Your message'
                 multiline
+                name={formFields.message}
+                onChange={handleChange}
+                required
                 rows='4'
+                value={values[formFields.message]}
+                variant='outlined'
               />
 
               <Button
                 variant='contained'
                 color='primary'
+                type='submit'
+                disabled={isLoading}
               >
                 Submit
               </Button>
@@ -118,8 +198,8 @@ const ContactForm = () => {
               variant='h6'
               component='p'
             >
-              Don't hesitate to send me a message or call me,
-              we can build on your proposition together and find the best possible outcome.
+              Don't hesitate to send me a message or call me.
+              Maybe we can start working together or simply just have a stimulating conversation.
             </Typography>
 
             <div
