44a2403531c91ee17a320f215e4963119f30cafe
Stephan Burger <stephanburger54@gmail.com>
2021-01-24 18:19:04 +0200

    about section done

--- src/sections/about/index.js
+++ src/sections/about/index.js
@@ -34,12 +34,8 @@ const AboutSection = () => {
           My name is Stephan Burger, a software developer with 4 years’ experience.
           I plan, design, code, deploy and maintain software solutions that businesses use to either boost their online presence or to promote productivity and efficiency.
           It can be as simple as a landing page or a cross-platform application that requires cross-device syncing and authentication.
-          I enjoy starting from the ground up, but I don’t reinvent the wheel.
-          Sometimes curiosity gets the best of me and I just start reinventing.
-        </Paragraph>
-
-        <Paragraph>
-          
+          I enjoy starting from the ground up while not reinventing the wheel but
+          sometimes curiosity gets the best of me and I just start reinventing.
         </Paragraph>
 
         <br/>
@@ -47,6 +43,85 @@ const AboutSection = () => {
         <HeaderOne>
           My skills and toolsets
         </HeaderOne>
+        <Paragraph>
+          Every project has its own set of problems and requirements.
+          Besides having a strong plan, one needs to be able to choose the right stack for the job.
+          Combined with having a strong foundation, no project is unconquerable.
+          That's why I've been pushing myself to keep on learning, making way for the stack I've come to love
+          and enjoy:
+        </Paragraph>
+
+        <HeaderTwo>
+          Languages:
+        </HeaderTwo>
+        <UnorderedList>
+          <ListItem>
+            Javascript
+          </ListItem>
+          <ListItem>
+            Typescript
+          </ListItem>
+          <ListItem>
+            NodeJS
+          </ListItem>
+          <ListItem>
+            Python
+          </ListItem>
+          <ListItem>
+            HTML and CSS
+          </ListItem>
+        </UnorderedList>
+
+        <HeaderTwo>
+          Tools and Libraries:
+        </HeaderTwo>
+        <UnorderedList>
+          <ListItem>
+            React
+          </ListItem>
+          <ListItem>
+            Gatsby
+          </ListItem>
+          <ListItem>
+            React Native
+          </ListItem>
+          <ListItem>
+            Electron
+          </ListItem>
+        </UnorderedList>
+
+        <HeaderTwo>
+          Databases:
+        </HeaderTwo>
+        <UnorderedList>
+          <ListItem>
+            MongoDB <b>[NoSQL]</b>
+          </ListItem>
+          <ListItem>
+            Postgres <b>[SQL]</b>
+          </ListItem>
+          <ListItem>
+            GunJS <b>[Graph]</b>
+          </ListItem>
+          <ListItem>
+            Graphql
+          </ListItem>
+        </UnorderedList>
+
+        <HeaderTwo>
+          Services:
+        </HeaderTwo>
+        <UnorderedList>
+          <ListItem>
+            Firebase
+          </ListItem>
+          <ListItem>
+            Digital Ocean
+          </ListItem>
+          <ListItem>
+            AWS
+          </ListItem>
+        </UnorderedList>
 
         <br />
 
@@ -57,7 +132,7 @@ const AboutSection = () => {
           There are 3 things I enjoy to the core; learning, tinkering, and skateboarding.
           When I’m not busy learning something new, you’ll find me tinkering away on something.
           And when things get too much, you might end up finding me at the local skatepark.
-          In each case, there’ll be music playing in the background.
+          Enjoying each one whilst listening to my favourite playlists.
         </Paragraph>
       </div>      
     </Section>
